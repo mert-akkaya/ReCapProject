d9be6f3929be2cfd8443d43edb9ea0be19b8742b
Mert AKKAYA <mertakkaya522@gmail.com>
2021-02-24 19:05:19 +0300

    Aop and Validation Aspect added

---
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,38 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Interceptors
+{
+    public class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation) { }
+        protected virtual void OnAfter(IInvocation invocation) { }
+        protected virtual void OnException(IInvocation invocation,Exception exception) { }
+        protected virtual void OnSuccess(IInvocation invocation) { }
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception exception)
+            {
+                isSuccess = false;
+                OnException(invocation,exception);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+            }
+            OnAfter(invocation);
+        }
+    }
+}
